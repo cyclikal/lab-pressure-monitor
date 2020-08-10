edf130128f40a7721cd725d83854c0bf98ce94d7
Aaron Beckman <aaronpbeckman@gmail.com>
2020-08-10 15:13:25 -0500

    Update test-api-data.py

--- test-api-data.py
+++ test-api-data.py
@@ -1,26 +1,33 @@
 import signal
 import sys
 import time
+import random
 
 import gspread
 
 
 def get_data():
-    lab_temp = 21.0  # Placeholder until sensors arrive
-    lab_hum = 65.5
-    glove_box_temp = 19.2
-    glove_box_hum = 33.2
-    glove_box_pressure = 1000  # Probably in millibar/hPa, would be most logical
+    lab_temp = random.uniform(20.0, 24.0)  # Placeholder until sensors arrive
+    lab_hum = random.uniform(40.0, 55.0)
+    glove_box_temp = random.uniform(20.0, 23.0)
+    glove_box_hum = random.uniform(15.0, 30.0)
+    # Probably in millibar/hPa, would be most logical
+    glove_box_pressure = random.uniform(1013.25, 1025.0)
 
-    return ['=NOW()', lab_temp, '=CONVERT(B2, "C", "F")', lab_hum, glove_box_temp, '=CONVERT(E2, "C", "F")', glove_box_hum, glove_box_pressure, '=(H2-1013.25) / 2.488']
+    return ['=TEXT(NOW(),"yyyy/mm/dd")', '=TEXT(NOW(),"hh:mm:ss")', lab_temp, '=CONVERT(C2, "C", "F")', lab_hum, glove_box_temp, '=CONVERT(F2, "C", "F")', glove_box_hum, glove_box_pressure, '=(I2-1013.25) / 2.488']
 
 
 def signal_handler(sig, frame):
+    data_sheet.update('A2', data_sheet.get(
+        'A2'), value_input_option="USER_ENTERED")
+    data_sheet.update('B2', data_sheet.get(
+        'B2'), value_input_option="USER_ENTERED")
     print('Exiting script...')
     sys.exit(0)
 
 
-loop_frequency = 3  # Seconds between each upload
+start_time = time.time()
+loop_frequency = 30  # Seconds between each upload
 signal.signal(signal.SIGINT, signal_handler)
 gc = gspread.service_account(filename='service_account.json')  # Held locally
 
@@ -30,6 +37,11 @@ data_sheet = gc.open("Test API Spreadsheet").worksheet("Raw Data")
 
 while True:
     data_sheet.insert_row(get_data(), 2, value_input_option="USER_ENTERED")
-    data_sheet.update('A2', data_sheet.get('A2'))
+    data_sheet.update('A2', data_sheet.get(
+        'A2'), value_input_option="USER_ENTERED")
+    data_sheet.update('B2', data_sheet.get(
+        'B2'), value_input_option="USER_ENTERED")
+    print(time.time() - start_time)
+    start_time = time.time()
 
     time.sleep(loop_frequency - time.time() % loop_frequency)
